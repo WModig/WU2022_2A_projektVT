b87d409dd26ccf7fcb40b553998cfa890a021ade
WModig <william.modig@elev.ga.ntig.se>
2022-03-09 13:37:36 +0100

    Grid

---
+++ js/script.js
@@ -0,0 +1,8 @@
+<ul id="nav-links" class="nav-active">
+
+<li> <a href="#">BILDER</a> </li>
+<li> <a href="#">KONTAKT</a> </li>
+<li> <a href="#">ÖVRIGT</a></li>
+<li> <a href="#">HEM</a></li>
+
+</ul>
